1b22945c4b95ecdbcdc415bfabee3f9ec32f1dec
Steve Morris <baramorf.com>
2023-02-12 11:40:09 +0000

    Going it alone 2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,16 +2,22 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
+  
   def new
     @article = Article.new
   end
 
   def create
     @article = Article.new
-    @article.save
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
   end
 
 end
