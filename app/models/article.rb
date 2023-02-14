81e598248f07ccd7829908ba3627820a2e6e14d3
Steve Morris <baramorf.com>
2023-02-14 13:50:00 +0000

    Deleting associated comments - adding # comments in code

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, :content, presence: true
-  has_many :comments
+  validates :title, :content, presence: true #TITLE AND CONTENT CANNOT BE BLANK
+  has_many :comments, dependent: :destroy #THIS ENSURES THE COMMENTS ARE REMOVED WITH THE ARTICLE
 end
